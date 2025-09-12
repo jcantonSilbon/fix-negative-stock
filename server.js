4c5a640e8515cdf1fd1b73c8398ecb61655bca6c
jcantonSilbon <jcanton@silbon.es>
2025-09-12 10:22:30 +0200

    go

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 8080;
 
 const SHOP  = process.env.SHOPIFY_SHOP;
 const TOKEN = process.env.SHOPIFY_ADMIN_TOKEN;
-const API_VERSION = process.env.SHOPIFY_API_VERSION || '2025-07';
+const API_VERSION = process.env.SHOPIFY_API_VERSION || '2024-04';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname  = path.dirname(__filename);
